75a98c01aa9d6957fd06bd4c7911643556702812
Kobe Brants <kobe.brants@telenet.be>
2024-10-18 10:56:24 +0200

    fix: CI/CD

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  if (mode === 'production') {
-    return {
-      plugins: [react()],
-      base: './',
-    };
-  }
-
-  return { plugins: [react()] };
+export default defineConfig({
+  plugins: [react()],
 });
