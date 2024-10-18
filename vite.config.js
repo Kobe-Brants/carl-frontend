5b30c8c4b6d7bd4f5667a419dff7c0b2a45a4f48
Kobe Brants <kobe.brants@telenet.be>
2024-10-18 09:22:46 +0200

    fix: CD

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/carl-frontend/',
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'production') {
+    return { plugins: [react()], base: './carl-frontend/' };
+  }
+
+  return { plugins: [react()] };
 });
