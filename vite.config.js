ce558569c684edc299b854f82c1626fb49a43337
Kobe Brants <kobe.brants@telenet.be>
2024-10-18 14:28:08 +0200

    fix: CI/CD

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_BASE_NAME || '/',
+  });
+};
